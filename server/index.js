e0a8cbdc0c5e7b8f978c317084c3029020c7fbed
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-01-11 23:39:43 -0500

    Updated posts.js (Controller)

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
 import postRoutes from "./routes/posts.js"
 import { register } from "./controllers/auth.js"
-import { createPost } from "./controllers/post.js"
+import { createPost } from "./controllers/posts.js"
 import { verifyToken } from "./middleware/auth.js"
 
 /** Config */
