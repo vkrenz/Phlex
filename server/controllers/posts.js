a1ae43694e8af0ce8371675f8a04cc6950becdfa
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-01-11 23:43:41 -0500

    Fixed imports

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,5 @@
-import Post from "../models/Post";
-import User from "../models/User";
+import Post from "../models/Post.js";
+import User from "../models/User.js";
 
 /** Create */
 export const createPost = async (req, res) => {
