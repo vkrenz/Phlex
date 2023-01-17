1e07070eb58441c6f701ee2fbba827f761562f74
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-01-16 22:17:11 -0500

    WIP `Formik`

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -25,3 +25,60 @@ const registerSchema = yup.object().shape({
     occupation: yup.string().required("required"),
     picture: yup.string().required("required")
 })
+
+const loginSchema = yup.object().shape({
+    email:yup.string().email("invalid email").required("required"),
+    password: yup.string().required("required")
+})
+
+const initialValuesRegister =  {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    location: "",
+    occupation: "",
+    picture: ""
+}
+
+const intialValuesLogin = {
+    email: "",
+    password: ""
+}
+
+const Form = () => {
+    const [pageType, setPageType] = useState("login")
+    const { palette } = useTheme()
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const isNonMobile = useMediaQuery("(min-width: 600px")
+    const isLogin = pageType === "login"
+    const isRegister = pageType === "register"
+    
+    const handleFormSubmit = async (values, onSubmitProps) => {}
+
+    return (
+        <Formik
+            onSubmit={handleFormSubmit}
+            initialValues={isLogin ? intialValuesLogin : initialValuesRegister}
+            validationSchema={isLogin ? loginSchema : registerSchema}
+        >
+            {({
+                values,
+                errors,
+                touched,
+                handleBlur,
+                handleChange,
+                handleSubmit,
+                setFieldValue,
+                resetForm
+            }) => {
+                <form onSubmit={handleSubmit}>
+                    
+                </form>
+            }}
+        </Formik>
+    )
+}
+
+export default Form
