1d67618d90ee0edf97dfa04a49ad4f9583e29799
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-02-02 22:18:53 -0500

    rebuilt `build`

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -154,7 +154,7 @@ const UserWidget = ({ userId, picturePath }) => {
 
                     <FlexBetween gap="1rem" mb="0.5rem">
                         <FlexBetween gap="1rem">
-                            <img src="../assets/twitter.png" alt="twitter" />
+                            <img src="/assets/twitter.png" alt="twitter" />
                             <Box>
                                 <Typography color={main} fontWeight="500">
                                     Twitter
@@ -169,7 +169,7 @@ const UserWidget = ({ userId, picturePath }) => {
 
                     <FlexBetween gap="1rem">
                         <FlexBetween gap="1rem">
-                            <img src="../assets/linkedin.png" alt="linkedin" />
+                            <img src="/assets/linkedin.png" alt="linkedin" />
                             <Box>
                                 <Typography color={main} fontWeight="500">
                                     Linkedin
